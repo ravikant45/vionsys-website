7cacd35110610f33474d7d990be59cef02e66c31
ranjitgund <ranjitgund@vionsys.com>
2024-04-19 18:28:55 +0530

    Contact us page started

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Conts from '../ui/contact/FirstSection'
+import Second from '../ui/contact/SecondSection'
 const page = () => {
-    return (
-        <div>Contact page</div>
-    )
+  return (
+    <div>
+      <Conts />
+      <Second/>
+    </div>
+  )
 }
 
 export default page
