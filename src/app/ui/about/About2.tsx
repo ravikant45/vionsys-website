21bd0f334565d2b7825579515cb3250a4d0ac088
SahilBankar-18 <Sahilbankar@vionsys.com>
2024-04-20 12:12:50 +0530

    AboutUs Page Updated

---
+++ src/app/ui/about/About2.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About2 = () => {
+  return (
+    <>
+      <div className="w-screen h-[40] border-2 flex justify-center items-center py-2">
+        <div className="h-[300px] w-[1200px] px-8 flex justify-around border-2">
+          <h1 className="h-[100px] w-[400px] border-2 text-3xl font-bold">Crafting Solutions For Global Innovators.</h1>
+          <p className="h-[150px] w-[600px] border-2">
+            Vionsys IT solutions INDIA Pvt. Ltd. is a leading IT company
+            providing comprehensive solutions. From software development and
+            cloud services to cybersecurity and IT consulting, we offer a wide
+            range of expertise. Our experienced team delivers tailored
+            solutions, ensuring efficiency, reliability, and scalability, to
+            help businesses thrive in the digital era.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About2;
