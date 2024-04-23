012b89fc5b5e079a067ef1b748806e4fa851524b
sagarvionsys <sagaryenkure@vionsys.com>
2024-04-23 14:12:51 +0530

    services structure created

---
+++ src/app/services/softwareDevelopment/page.tsx
@@ -0,0 +1,12 @@
+import ServicesTemplateOne from "@/app/ui/services/ServicesTemplateOne";
+import React from "react";
+
+const softwareDevlopment = () => {
+  return (
+    <main className="pt-16">
+      <ServicesTemplateOne />
+    </main>
+  );
+};
+
+export default softwareDevlopment;
