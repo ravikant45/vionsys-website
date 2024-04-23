b96cbf165aefc9ba8c4400ed713e493a30244cdd
SahilBankar-18 <Sahilbankar@vionsys.com>
2024-04-23 13:32:20 +0530

    About Page Updated

--- src/app/ui/about/AboutUs.tsx
+++ src/app/ui/about/AboutUs.tsx
@@ -2,28 +2,25 @@
 import React from "react";
 import { BsTelephoneOutboundFill } from "react-icons/bs";
 import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 
 const AboutUs = () => {
   return (
     <>
       <div className="w-screen min-h-screen">
-        <div
-          className="h-screen w-screen bg-cover bg-center relative"
-          style={{
-            backgroundImage:
-              "url('https://img.freepik.com/premium-photo/business-meeting_53876-91042.jpg')",
-          }}
-        >
-          <div className="h-screen border-2 w-full flex flex-col justify-start py-4 lg:items-start">
-            {/* Image Icon */}
-            <img
-              src="https://png.pngtree.com/png-vector/20221122/ourmid/pngtree-thinking-man-illustration-with-question-mark-and-light-bulb-for-business-png-image_6475461.png"
-              className="h-32 w-32 mt-10 sm:h-48 sm:w-48"
-              alt=""
-            />
+        <div className="h-screen w-screen relative">
+          <video
+            autoPlay
+            loop
+            muted
+            className="absolute inset-0 object-cover w-full h-full"
+          >
+            <source src="/assets/About/AboutBg.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
 
+          <div className="h-screen absolute z-1 w-full flex flex-col justify-center py-4 lg:items-start">
             {/* Text Content with Animation */}
             <motion.div
               initial={{ opacity: 0, y: 50 }}
@@ -31,7 +28,7 @@ const AboutUs = () => {
               transition={{ duration: 1 }}
               className="text-center mt-5 px-8 sm:mt-10 sm:px-20"
             >
-              <h1 className="text-zinc-600 text-3xl md:text-7xl">
+              <h1 className="text-white text-3xl md:text-7xl">
                 Ready To <br className="sm:hidden" />
                 <span className="block">Transform Your Ideas </span>
                 Into Reality?
@@ -39,15 +36,14 @@ const AboutUs = () => {
             </motion.div>
 
             {/* Contact Us Button */}
-            <div className="h-12 flex justify-center md:absolute md:left-80 md:top-[530px] items-center bg-white text-blue-700 hover:bg-blue-700 hover:text-white border-2 border-blue-700 rounded-full shadow-xl sm:p-3">
+            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
               <Link href="/contact" passHref>
-                <button className="font-bold transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-400">
-                  <BsTelephoneOutboundFill className="inline-block mr-2 text-xl" />
-                  Get In Touch
-                </button>
+                <Button>
+                  Call Us{" "}
+                  <BsTelephoneOutboundFill className="inline-block ml-2 text-sm" />
+                </Button>
               </Link>
             </div>
-
           </div>
         </div>
       </div>
--- src/app/ui/about/TypeWriterDemo.tsx
+++ src/app/ui/about/TypeWriterDemo.tsx
@@ -5,27 +5,33 @@ import { Button } from '@/components/ui/button';
 export function TypewriterDemo() {
   const words = [
     {
-      text: "Considering",
-      className: "text-orange-500 dark:text-blue-500",
+      text: "Intrested",
+      
     },
     {
-      text: "a",
+      text: "in",
     },
     {
       text: "Career",
+      className: "text-orange dark:text-blue",
     },
     {
-      text: "with",
+      text: "opportunities",
+      className: "text-orange dark:text-blue",
     },
     {
-      text: "us?.",
+        text: "with",
+      },
+    {
+      text: "us?",
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-center h-[40rem] ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-base  mb-10">
-        The road to freedom starts from here
-      </p>
+    <div className="flex flex-col items-center bg-cover justify-center h-[40rem] " 
+        style={{
+        backgroundImage:
+          "url('https://png.pngtree.com/background/20210709/original/pngtree-simple-e-commerce-web-page-business-picture-image_935500.jpg')",
+      }}>
       <TypewriterEffect words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
       <Button>Call Us</Button>
