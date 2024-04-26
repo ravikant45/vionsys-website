93b6d1589717326ef10cd6260d6e474a16ddda1b
ravikant <Ravikantwaghmare@vionsys.com>
2024-04-26 15:00:57 +0530

    pages created in industries section or route

--- src/app/industries/page.tsx
+++ src/app/industries/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import FirstSection from '../ui/industries/FirstSection'
+import SecondSection from '../ui/industries/SecondSection'
+import ThirdSection from '../ui/industries/ThirdSection'
 
 const page = () => {
     return (
-        <div>page</div>
+        <div className='pt-16 overflow-x-hidden'>
+            {/* First Section */}
+            <FirstSection />
+
+            {/* Second Section */}
+            <SecondSection />
+
+            {/* Third Section */}
+            <ThirdSection />
+        </div>
     )
 }
 
