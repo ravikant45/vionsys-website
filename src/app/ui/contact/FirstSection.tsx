34014c000f5c8ef2071e3f733dde11cab1b66e22
ranjitgund <ranjitgund@vionsys.com>
2024-04-26 14:54:37 +0530

    Changes in Background images of Contact us page

--- src/app/ui/contact/FirstSection.tsx
+++ src/app/ui/contact/FirstSection.tsx
@@ -1,66 +1,66 @@
 import React from "react";
 import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 
-  export function TypewriterEffectDemo() { }
-  const words = [
-    {
-      text: "Think",
-      className: "text-white ",
-    },
-    {
-      text: "Beyond",
-      className: "text-white",
-    },
-    {
-      text: "With",
-      className: "text-white",
-    },
-    {
-      text: "Vionsys",
-      className: "text-white",
-    },
-    {
-      text: "Redefining ",
-      className: "text-white ",
-    },
-    {
-      text: "IT",
-      className: "text-white",
-    },
-    {
-      text: "Solutions",
-      className: "text-white",
-    },
-    {
-      text: ".",
-      className: "text-white",
-    },
-    {
-      text: ".",
-      className: "text-white",
-    },
-    {
-      text: ".",
-      className: "text-white",
-    },
-  ];
+export function TypewriterEffectDemo() {}
+const words = [
+  {
+    text: "Think",
+    className: "text-white ",
+  },
+  {
+    text: "Beyond",
+    className: "text-white",
+  },
+  {
+    text: "With",
+    className: "text-white",
+  },
+  {
+    text: "Vionsys",
+    className: "text-white",
+  },
+  {
+    text: "Redefining ",
+    className: "text-white ",
+  },
+  {
+    text: "IT",
+    className: "text-white",
+  },
+  {
+    text: "Solutions",
+    className: "text-white",
+  },
+  {
+    text: ".",
+    className: "text-white",
+  },
+  {
+    text: ".",
+    className: "text-white",
+  },
+  {
+    text: ".",
+    className: "text-white",
+  },
+];
 
-  const Conts = () => {
-    return (
-      <div>
-        <div className="">
-          <div className="w-full h-full relative flex">
-            <img
-              className="w-full h-full  object-cover bg-no-repeat animate-"
-              src="https://img.freepik.com/free-photo/blue-color-cinematic-style-modern-open-plan-office_1409-7481.jpg?t=st=1714036019~exp=1714039619~hmac=e6a190d3ccbbf69319653861f94c5767554953927ce3f030c337e0b74da77165&w=1060"
-              alt=""
-            />
-            <div className="absolute top-0 left-0 right-20  w-full h-full flex items-center">
-              <TypewriterEffect words={words} />
-            </div>
+const Conts = () => {
+  return (
+    <div>
+      <div className="">
+        <div className="w-full h-full relative flex bg-black">
+          <img
+            className="w-full h-full  object-cover bg-no-repeat opacity-55 "
+            src="https://img.freepik.com/free-photo/interior-workspace-inspired-by-technology-innovation_1409-7691.jpg?t=st=1714123232~exp=1714126832~hmac=15c4e82539d76b79b7f97f84ed72d5634cbdfbd566e8b3b31b08243316ba1b07&w=1060"
+            alt=""
+          />
+          <div className="absolute top-0 left-0 right-20  w-full text-2xl h-full flex items-center">
+            <TypewriterEffect words={words} />
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 export default Conts;
