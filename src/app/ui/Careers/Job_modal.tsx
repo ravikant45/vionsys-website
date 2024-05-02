642b89a1296fca5b4710ee8f1f5e43703fd8be89
sagarvionsys <sagaryenkure@vionsys.com>
2024-05-02 17:18:40 +0530

    Changed ui & added some features

--- src/app/ui/Careers/Job_modal.tsx
+++ src/app/ui/Careers/Job_modal.tsx
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import { Modal } from "antd";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import toast from "react-hot-toast";
@@ -30,7 +38,13 @@ const formSchema = z.object({
   attachments: z.array(fileSchema),
 });
 
-const Job_modal = ({ isModalOpen, setisModalOpen }: { isModalOpen: any; setisModalOpen: any; }) => {
+const Job_modal = ({
+  isModalOpen,
+  setisModalOpen,
+}: {
+  isModalOpen: any;
+  setisModalOpen: any;
+}) => {
   // Initialize useForm hook
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -40,7 +54,6 @@ const Job_modal = ({ isModalOpen, setisModalOpen }: { isModalOpen: any; setisMod
       position: "",
       experience: "",
       attachments: undefined,
-
     },
   });
   const [isPending, setIsPending] = useState<boolean>(false);
@@ -58,20 +71,19 @@ const Job_modal = ({ isModalOpen, setisModalOpen }: { isModalOpen: any; setisMod
         values,
         {
           headers: {
-            "Content-Type": "form-data"
-          }
+            "Content-Type": "form-data",
+          },
         }
       );
 
       setIsPending(false);
-      toast.success("Message sent successfully!");
-      console.log("Response:", response.data);
+      toast.success("Job Application submitted");
       form.reset(); // Reset the form after successful submission
     } catch (error) {
       const err = error as AxiosError;
       console.error("Error:", err.response?.data || err.message);
       setIsPending(false);
-      toast.error(err.message);
+      toast.error(err?.message || "failed to send Job Application");
     }
     form.reset();
   };
@@ -138,7 +150,10 @@ const Job_modal = ({ isModalOpen, setisModalOpen }: { isModalOpen: any; setisMod
                         <FormItem>
                           <FormLabel>Designation</FormLabel>
                           <FormControl>
-                            <Input placeholder="Enter Designation to apply" {...field} />
+                            <Input
+                              placeholder="Enter Designation to apply"
+                              {...field}
+                            />
                           </FormControl>
                           <FormDescription></FormDescription>
                           <FormMessage />
@@ -156,16 +171,18 @@ const Job_modal = ({ isModalOpen, setisModalOpen }: { isModalOpen: any; setisMod
                         <FormItem>
                           <FormLabel>Experience in years</FormLabel>
                           <FormControl>
-                            <Input type="number" placeholder="Experience in years" {...field} />
+                            <Input
+                              type="number"
+                              placeholder="Experience in years"
+                              {...field}
+                            />
                           </FormControl>
                           <FormDescription></FormDescription>
                           <FormMessage />
                         </FormItem>
                       )}
                     />
-
                   </div>
-
                 </div>
 
                 {/* attachments */}
@@ -186,14 +203,15 @@ const Job_modal = ({ isModalOpen, setisModalOpen }: { isModalOpen: any; setisMod
                               Promise.all(
                                 Array.from(files).map(async (file) => ({
                                   filename: file.name,
-                                  content: Buffer.from(await file.arrayBuffer()).toString('base64'),
+                                  content: Buffer.from(
+                                    await file.arrayBuffer()
+                                  ).toString("base64"),
                                 }))
                               ).then((filesArray) => {
                                 field.onChange(filesArray);
                               });
                             }
                           }}
-
                         />
                       </FormControl>
                       <FormDescription></FormDescription>
@@ -203,19 +221,32 @@ const Job_modal = ({ isModalOpen, setisModalOpen }: { isModalOpen: any; setisMod
                 />
 
                 <div className="mb-6">
-                  {
-                    !isPending ? (<Button type="submit" className="flex justify-center">
+                  {!isPending ? (
+                    <Button type="submit" className="flex justify-center">
                       Apply
-                    </Button>) : (
-                      <Button type="submit" className="flex justify-center">
-                        <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-                        </svg>
-                        Sending..
-                      </Button>
-                    )
-                  }
+                    </Button>
+                  ) : (
+                    <Button type="submit" className="flex justify-center">
+                      <svg
+                        aria-hidden="true"
+                        role="status"
+                        className="inline w-4 h-4 me-3 text-white animate-spin"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="#E5E7EB"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                      Sending..
+                    </Button>
+                  )}
                 </div>
               </form>
             </Form>
