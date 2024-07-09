e8027ded27710ab168ea6032aa6356372dca4c57
sagarvionsys <sagaryenkure@vionsys.com>
2024-07-09 15:44:38 +0530

    meta data added to blog and case study page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,21 +3,14 @@ import { GeistSans } from "geist/font/sans";
 import Sidebar from "../ui/dashboard/admin/Sidebar";
 
 export default function RootLayout({
-    children
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <div className={GeistSans.className}>
-            <Providers>
-                <div className="bg-white overflow-x-hidden w-screen">
-                    <Sidebar />
-                    <div className="flex-1 md:ml-40 ml-16 z-0">
-                        {children}
-                    </div>
-                </div>
-            </Providers>
-        </div>
-    )
+  return (
+    <div className={GeistSans.className}>
+      <Sidebar />
+      <Providers>{children}</Providers>
+    </div>
+  );
 }
-
--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -2,20 +2,13 @@ import Providers from "@/utils/Providers";
 import { GeistSans } from "geist/font/sans";
 
 export default function RootLayout({
-    children
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <div className={GeistSans.className}>
-            <Providers>
-                <div className="flex h-screen">
-                    <div className="flex-1 overflow-auto">
-                        {children}
-                    </div>
-                </div>
-            </Providers>
-        </div>
-    )
+  return (
+    <div className={GeistSans.className}>
+      <Providers>{children}</Providers>
+    </div>
+  );
 }
-
