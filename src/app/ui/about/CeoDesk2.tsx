d63197d4cc808af5f859f27bd78da29f5044e489
Asmita <Asmitadhole@vionsys.com>
2024-08-05 18:29:24 +0530

    Worked on responsiveness and other changes in About section & in Education & Field Industry

--- src/app/ui/about/CeoDesk2.tsx
+++ src/app/ui/about/CeoDesk2.tsx
@@ -52,23 +52,23 @@ const CeoDesk2 = () => {
 
   return (
     <div className="w-full flex flex-col items-center py-14 bg-gradient-to-l from-[#215cbc] via-blue-100 to-gray-100">
-      <div className="w-[70vw]  font-light tracking-wide">
+      <div className="md:w-[70vw] w-[90vw] font-light tracking-wide">
         <h2 className="text-3xl font-bold text-[#215cbc]">From Leadership's Desk</h2>
         <div className="pt-1 rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex-grow mt-3 mb-7 w-28"></div>
       </div>
-      <div className="w-[70vw] overflow-hidden relative">
+      <div className="md:w-[70vw] w-[90vw] overflow-hidden relative">
         <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
           {CEODesk.map((item, index) => (
-            <div key={index} className="min-w-full flex flex-row justify-center items-start font-light tracking-wide" style={{ fontFamily: 'Open Sans, sans-serif' }}>
-              <div className="w-1/2">
+            <div key={index} className="min-w-full flex md:flex-row flex-col justify-center items-start font-light tracking-wide" style={{ fontFamily: 'Open Sans, sans-serif' }}>
+              <div className="md:w-1/2 md:order-1 order-2 md:py-0 mt-7">
                 <div className="text-left mx-auto">
-                  <div className="mb-4 text-black flex flex-col items-start pr-10">
+                  <div className="mb-4 text-black flex flex-col items-start md:pr-10">
                     <Image src={colon} alt='' height={100} width={100} className="py-4 rounded-sm" />
                     <p className="text-base leading-6">"{item.Message}"</p>
                   </div>
                 </div>
               </div>
-              <div className="w-1/2 flex flex-col items-start">
+              <div className="md:w-1/2 md:order-2 order-1 flex flex-col md:items-start items-center">
                 <div className="flex items-start pb-6">
                   <Image
                     src={item.Img}
@@ -78,8 +78,8 @@ const CeoDesk2 = () => {
                     className="w-48 h-48 border rounded-full"
                   />
                 </div>
-                <div>
-                  <div className="border-l-2 border-white h-20 pl-10">
+                <div className="">
+                  <div className="md:border-l-2 md:border-white md:h-20 md:pl-10 flex flex-col md:items-start items-center">
                   <p className="font-semibold text-xl">{item.Name}</p>
                   <p className="font-medium">{item.Position}</p>
                   <p className="font-medium">{item.Location}</p>
