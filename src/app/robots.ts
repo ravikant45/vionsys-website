a95f578b638a150bbcb0449c36ecea1ab52a1f0a
sagarvionsys <sagaryenkure@vionsys.com>
2024-08-07 17:23:27 +0530

    update the robot file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,14 @@
 import { MetadataRoute } from "next";
-const BaseUrl = process.env.DOMAIN;
+
+const BaseUrl = process.env.DOMAIN || "";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: "*",
         allow: "/",
+        disallow: ["/admin/*"],
       },
     ],
     sitemap: `${BaseUrl}/sitemap.xml`,
