e010ee5d7307fa53aa0e1ecb7b80a4da656e9c4b
sagarvionsys <sagaryenkure@vionsys.com>
2024-08-14 11:55:28 +0530

    baseurl update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,6 +8,7 @@ import {
   staticPages,
 } from "@/helper/sitemapHelpers";
 
+export const BaseUrl = process.env.DOMAIN || "";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   // Generate sitemaps for services and industries
   const servicesSitemap = generateSitemapEntries(ServicesLinks);
