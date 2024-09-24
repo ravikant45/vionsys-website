e20ce8e29574ef17ec90ade004f97af31b458de2
Sahil1849 <dev321.net@gmail.com>
2024-09-24 11:28:24 +0530

    Updated IT Staffing services Landing page(Offerings, whatWeServe), Functions created for sending email with attachment.

--- src/app/api/sendEmailWithFile/route.ts
+++ src/app/api/sendEmailWithFile/route.ts
@@ -1,32 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
-import { transporter } from "@/lib/transporter";
+import useSendEmailWithAttachments from "@/lib/useSendEmailWithAttachments";
 
 export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const html = `
+  <h2>Vionsys Website Job application for ${data.position}</h2>
+  <h3>Applicant Details:</h3>
+   <h3><span style="font-weight: bold;">Name:</span> <span style="font-weight: lighter;">${data.name}</span></h3>
+   <h3><span style="font-weight: bold;">Email:</span> <span style="font-weight: lighter;">${data.email}</span></h3>
+   <h3><span style="font-weight: bold;">Experience:</span> <span style="font-weight: lighter;">${data.experience}</span></h3>
+`;
   try {
-    const { name, email, position, experience, attachments } = await req.json();
-    const mailOptions = {
-      from: process.env.GMAIL_USER!,
-      to: "hr@vionsys.com",
+    await useSendEmailWithAttachments({
+      data,
+      sendTo: "hr@vionsys.com",
       subject: "Vionsys Website career application",
-      html: `
-           <h2>Vionsys Website Job application for ${position}</h2>
-           <h3>Applicant Details:</h3>
-            <h3><span style="font-weight: bold;">Name:</span> <span style="font-weight: lighter;">${name}</span></h3>
-            <h3><span style="font-weight: bold;">Email:</span> <span style="font-weight: lighter;">${email}</span></h3>
-            <h3><span style="font-weight: bold;">Experience:</span> <span style="font-weight: lighter;">${experience}</span></h3>
-        `,
-      attachments: attachments.map((file: any) => ({
-        filename: file.filename,
-        content: Buffer.from(file.content, "base64"),
-      })),
-    };
-    await transporter.sendMail(mailOptions);
+      html,
+    });
     return NextResponse.json({
       message: "Email sent successfully",
       success: true,
     });
   } catch (error) {
-    console.error("Error sending email:", error);
     return Response.json({
       message: "Something went wrong!!!",
       success: false,
