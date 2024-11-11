0b697faa4cf9b5972effcbf6a5506e0566c75ca6
sagar yenkure <sagaryenkure@vionsys.com>
2024-11-11 18:02:56 +0530

    chatbot ui added

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -1,17 +1,17 @@
 "use client";
-import React from 'react';
+import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    )
-}
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} position="top" />
+    </QueryClientProvider>
+  );
+};
 
 export default Providers;
